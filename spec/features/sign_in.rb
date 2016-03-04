7f00a1675c1226be3d139df0451cfda5851cb858
José Antonio Ruiz del Moral Cervantes <joseantoniordlmc@gmail.com>
2016-03-03 21:09:16 -0600

    Sign In Capybara Test

---
+++ spec/features/sign_in.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe "The Sign In process", :type => :feature do
+
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+
+  it "signs in the user" do
+    visit "users/sign_in"
+    fill_in "user[email]", with: @user.email
+    fill_in "user[password]", with: "su$eppm01"
+    click_on "Log in"
+
+    expect(page).to have_content 'Log out'
+  end
+end
