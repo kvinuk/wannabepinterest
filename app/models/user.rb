e7fa1ee43b7e76ffe0e0b4265f6aed104738944c
kvinuk <kevinomuro@gmail.com>
2016-03-07 19:35:24 -0600

    rspec for board

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+         has_many :boards
+
   validates :name, :username, presence: true
   validates :username, uniqueness: true
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
+RSpec.describe Board, :type => :model do
   let(:user){ FactoryGirl.build(:user) }
   subject { user }
 
   it { should validate_presence_of(:name)}
   it { should validate_presence_of(:username)}
   it { should validate_uniqueness_of(:username)}
-  it { should have_many(:boards) }
 end
