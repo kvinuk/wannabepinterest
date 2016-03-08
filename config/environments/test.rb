06a817ce78d32426fee2ea86661965c4d97a7fc7
Abraham Kuri <kurenn@icalialabs.com>
2016-03-07 19:14:14 -0600

    Refactors the user registration spec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.allow_concurrency = false
+
+  # Capybara config
+  Capybara.javascript_driver = :selenium
 end
--- spec/features/registration.rb
+++ spec/features/registration.rb
@@ -1,26 +1,33 @@
 require "rails_helper"
 
 describe "the registration process", type: :feature do
-
-  it "register the user" do
-    visit "/users/sign_up"
-    fill_in "user[name]", with: "Abraham Kuri"
-    fill_in "user[username]", with: "kurenn"
-    fill_in "user[email]", with: "kurenn@icalialabs.com"
-    fill_in "user[password]", with: "12345678"
-    fill_in "user[password_confirmation]", with: "12345678"
-    click_on "Sign up"
-    expect(page).to have_content 'Log out'
+  before do
+    @user = FactoryGirl.build :user
   end
 
-  it "does not register the user, due to missing username" do
-    visit "/users/sign_up"
-    fill_in "user[name]", with: "Abraham Kuri"
-    fill_in "user[email]", with: "kurenn@icalialabs.com"
-    fill_in "user[password]", with: "12345678"
-    fill_in "user[password_confirmation]", with: "12345678"
-    click_on "Sign up"
-    expect(page).to have_content "Username can't be blank"
+  context "when the user is correct" do
+    it "register the user" do
+      visit "/users/sign_up"
+      fill_in "user[name]", with: @user.name
+      fill_in "user[username]", with: @user.username
+      fill_in "user[email]", with: @user.email
+      fill_in "user[password]", with: "12345678"
+      fill_in "user[password_confirmation]", with: "12345678"
+      click_on "Sign up"
+      expect(page).to have_content 'Log out'
+    end
+  end
+
+  context "when the user is not correct" do
+    it "does not register the user, due to missing username" do
+      visit "/users/sign_up"
+      fill_in "user[name]", with: @user.name
+      fill_in "user[email]", with: @user.email
+      fill_in "user[password]", with: "12345678"
+      fill_in "user[password_confirmation]", with: "12345678"
+      click_on "Sign up"
+      expect(page).to have_content "Username can't be blank"
+    end
   end
 
 end
