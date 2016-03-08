09481de816bef91a9e0cd309509b1a57d74ba35c
Abraham Kuri <kurenn@icalialabs.com>
2016-03-07 18:47:33 -0600

    Adds registration step for user

---
+++ spec/features/registration.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "the registration process", type: :feature do
+
+  it "register the user" do
+    visit "/users/sign_up"
+    fill_in "user[name]", with: "Abraham Kuri"
+    fill_in "user[username]", with: "kurenn"
+    fill_in "user[email]", with: "kurenn@icalialabs.com"
+    fill_in "user[password]", with: "12345678"
+    fill_in "user[password_confirmation]", with: "12345678"
+    click_on "Sign up"
+    expect(page).to have_content 'Log out'
+  end
+end
