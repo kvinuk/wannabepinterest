cb8c0405f6ecaba168ec3cd7b901eefce7102778
Abraham Kuri <kurenn@icalialabs.com>
2016-03-07 18:58:08 -0600

    Adds failing test for user registration

--- spec/features/registration.rb
+++ spec/features/registration.rb
@@ -12,4 +12,15 @@ describe "the registration process", type: :feature do
     click_on "Sign up"
     expect(page).to have_content 'Log out'
   end
+
+  it "does not register the user, due to missing username" do
+    visit "/users/sign_up"
+    fill_in "user[name]", with: "Abraham Kuri"
+    fill_in "user[email]", with: "kurenn@icalialabs.com"
+    fill_in "user[password]", with: "12345678"
+    fill_in "user[password_confirmation]", with: "12345678"
+    click_on "Sign up"
+    expect(page).to have_content "Username can't be blank"
+  end
+
 end
