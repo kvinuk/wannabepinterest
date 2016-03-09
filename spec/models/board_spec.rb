6206cb583cbff5d7b8a7b343f417f82720e30d9b
kvinuk <kevinomuro@gmail.com>
2016-03-09 11:45:55 -0600

    test commit

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -7,4 +7,4 @@ RSpec.describe Board, :type => :model do
   it { should validate_presence_of(:name)}
   it { should validate_presence_of(:username)}
   it { should validate_uniqueness_of(:username)}
-end
+end 
