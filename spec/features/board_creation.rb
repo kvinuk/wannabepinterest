3fa87977e6b1d2ed88d6e984e9f51362a263b0c8
Abraham Kuri <kurenn@icalialabs.com>
2016-03-14 19:01:18 -0600

    Adds board creation form for user

---
+++ spec/features/board_creation.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "The board creation process", :type => :feature do
+  before do
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
+
+  it "signs in the user" do
+    visit "/boards/new"
+    fill_in "board[name]", with: "My new board"
+    click_button "Save"
+    expect(current_path).to eql "/"
+  end
+end
