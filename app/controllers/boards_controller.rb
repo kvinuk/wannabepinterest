63964d4d92e6a87f6a4f2884a72daff5aec7af96
Abraham Kuri <kurenn@icalialabs.com>
2016-03-17 18:14:13 -0600

    Adds destroy link for boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -19,6 +19,12 @@ class BoardsController < ApplicationController
     end
   end
 
+  def destroy
+    board = Board.find(params[:id])
+    board.destroy
+    redirect_to boards_path
+  end
+
   private
 
     def board_params
